6e0b1963a8aec90583ec1ef75792e0621cc2035a
Heat Koemnak <heatkimnak@gmail.com>
2023-11-15 23:46:09 +0700

    update index and env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = 3000;
 
 app.use(express.json());
@@ -10,6 +12,16 @@ app.use(cors());
 app.get('/users', (req, res) => {
   res.send('Hello users');
 });
+// MONGO_URL = 'mongodb+srv://heatkimnak:EnITmN2hkFbRiTzy@cluster0.ffsxyzk.mongodb.net/mernchat?retryWrites=true&w=majority';
+const MongoDB_URL = process.env.MONGO_URL;
+mongoose
+  .connect(MongoDB_URL)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((err) => {
+    console.log('failed to connect mongodb', err);
+  });
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
