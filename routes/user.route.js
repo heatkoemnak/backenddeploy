b7fe8b72c0d09fa2a8696290d7a771c7f3f0882d
Heat Koemnak <heatkimnak@gmail.com>
2023-11-16 01:44:28 +0700

    updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,8 @@ const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
+
+const TOKEN_SECRET = 'jkmknsxanasasnklkmoasa';
 router.get('/users', async (req, res) => {
   try {
     const users = await userModel.find();
@@ -12,48 +14,56 @@ router.get('/users', async (req, res) => {
   }
 });
 
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password)
-    return res
-      .status(400)
-      .send({ error: 'You must provide a username and password' });
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
+router.get('/profile', async (req, res) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).json({ error: 'Unauthorized' });
   try {
-    const usernameExist = await userModel.findOne({ username });
-    if (usernameExist)
-      return res.status(400).send({ error: 'Username already exists' });
-    const user = new userModel({
-      username: username,
-      password: hashedPassword,
+    jwt.verify(token, TOKEN_SECRET, (err, userData) => {
+      if (err) return res.status(400).json(err);
+      res.status(200).json(userData);
     });
-    await user.save();
-    // if (user) {
-    //   jwt.sign(
-    //     { userId: user._id, username },
-    //     process.env.TOKEN_SECRET,
-    //     (err, token) => {
-    //       if (err) return res.status(400).json(err);
-    //       res
-    //         .cookie('token', token, {
-    //           sameSite: 'none',
-    //           secure: true,
-    //         })
-    //         .status(201)
-    //         .json({
-    //           user: user,
-    //         });
-    //     }
-    //   );
-    // }
-    res.status(201).json({ user: user });
   } catch (err) {
     res.status(400).json(err);
   }
-});
+}),
+  router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password)
+      return res
+        .status(400)
+        .send({ error: 'You must provide a username and password' });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    try {
+      const usernameExist = await userModel.findOne({ username });
+      if (usernameExist)
+        return res.status(400).send({ error: 'Username already exists' });
+      const user = new userModel({
+        username: username,
+        password: hashedPassword,
+      });
+      await user.save();
+      if (user) {
+        jwt.sign({ userId: user._id, username }, TOKEN_SECRET, (err, token) => {
+          if (err) return res.status(400).json(err);
+          res
+            .cookie('token', token, {
+              sameSite: 'none',
+              secure: true,
+            })
+            .status(201)
+            .json({
+              user: user,
+            });
+        });
+      }
+      res.status(201).json({ user: user });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
@@ -65,26 +75,20 @@ router.post('/login', async (req, res) => {
     const user = await userModel.findOne({ username });
     if (!user) return res.status(400).send({ error: 'User not found' });
     const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
+    if (!validPassword)
       return res.status(400).send({ error: 'Invalid password' });
-    } else {
-      jwt.sign(
-        { userId: user._id, username, userProfile: user.profilePicture },
-        process.env.TOKEN_SECRET,
-        (err, token) => {
-          if (err) return res.status(400).json(err);
-          res
-            .cookie('token', token, {
-              sameSite: 'none',
-              secure: true,
-            })
-            .status(201)
-            .json({
-              user: user,
-            });
-        }
-      );
-    }
+    jwt.sign({ userId: user._id, username }, TOKEN_SECRET, (err, token) => {
+      if (err) return res.status(400).json(err);
+      res
+        .cookie('token', token, {
+          sameSite: 'none',
+          secure: true,
+        })
+        .status(201)
+        .json({
+          user: user,
+        });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
