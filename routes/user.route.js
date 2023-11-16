94a1fcd36aeba4198b609a126f2d99bbacc951dd
Heat Koemnak <heatkimnak@gmail.com>
2023-11-16 17:06:57 +0700

    update 1

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
-const TOKEN_SECRET = 'jkmknsxanasasnklkmoasa';
+// const TOKEN_SECRET = 'jkmknsxanasasnklkmoasa';
 router.get('/users', async (req, res) => {
   try {
     const users = await userModel.find();
@@ -18,7 +18,7 @@ router.get('/profile', async (req, res) => {
   const token = req.cookies?.token;
   if (!token) return res.status(401).json({ error: 'Unauthorized' });
   try {
-    jwt.verify(token, TOKEN_SECRET, (err, userData) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, userData) => {
       if (err) return res.status(400).json(err);
       res.status(200).json(userData);
     });
@@ -45,20 +45,20 @@ router.get('/profile', async (req, res) => {
         password: hashedPassword,
       });
       await user.save();
-      // if (user) {
-      //   jwt.sign({ userId: user._id, username }, TOKEN_SECRET, (err, token) => {
-      //     if (err) return res.status(400).json(err);
-      //     res
-      //       .cookie('token', token, {
-      //         sameSite: 'none',
-      //         secure: true,
-      //       })
-      //       .status(201)
-      //       .json({
-      //         user: user,
-      //       });
-      //   });
-      // }
+      if (user) {
+        jwt.sign({ userId: user._id, username }, process.env.TOKEN_SECRET, (err, token) => {
+          if (err) return res.status(400).json(err);
+          res
+            .cookie('token', token, {
+              sameSite: 'none',
+              secure: true,
+            })
+            .status(201)
+            .json({
+              user: user,
+            });
+        });
+      }
       res.status(201).json({ user: user });
     } catch (err) {
       res.status(400).json(err);
