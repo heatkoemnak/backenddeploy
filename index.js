723a04c56f3b4705dbb743ba87f7413e65dcb5d9
Heat Koemnak <heatkimnak@gmail.com>
2023-11-17 18:56:15 +0700

    update server

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://flychat.vercel.app',
+    origin: 'https://flachat.vercel.app',
     credentials: true,
   })
 );
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://flychat.vercel.app',
+    origin: 'https://flachat.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
