4dab815794a69b0ba822aa0c33e7ba41c6a25d4d
Heat Koemnak <heatkimnak@gmail.com>
2023-11-17 18:49:26 +0700

    update

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://flychat.vercel.app',
     credentials: true,
   })
 );
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://flychat.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
